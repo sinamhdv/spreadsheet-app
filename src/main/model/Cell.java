e00533fa5347de737d00856860ca208b62bc3962
sinamhdv <sinamhdv@gmail.com>
2024-02-14 14:56:22 -0800

    add basic Cell class to model

---
+++ src/main/model/Cell.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Cell {
+    private Object data;
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
