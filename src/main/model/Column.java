6cb7c9601a7894ae8493054c3127d6a9bf291aba
sinamhdv <sinamhdv@gmail.com>
2024-02-14 15:09:37 -0800

    add column class

---
+++ src/main/model/Column.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Column {
+    private String name;
+    private DataType type;
+
+    public Column(String name, DataType type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public DataType getType() {
+        return type;
+    }
+}
