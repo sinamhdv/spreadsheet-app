1e0b3261d530df1424345084d915d386c4a11efb
sinamhdv <sinamhdv@gmail.com>
2024-02-15 10:40:51 -0800

    add class for error messages from model to ui

---
+++ src/main/model/ErrorMessage.java
@@ -0,0 +1,17 @@
+package model;
+
+// enum representing an error message returned from model to ui
+public enum ErrorMessage {
+    // Main Menu
+    NAME_NOT_FOUND("Name not found");
+
+    private final String text;
+
+    private ErrorMessage(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
