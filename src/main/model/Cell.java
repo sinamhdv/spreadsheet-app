9be78a4883425a4bc1686b3c3878c7dcb639cd23
sinamhdv <sinamhdv@gmail.com>
2024-02-15 13:09:56 -0800

    add Cell.of to create cells with different types

--- src/main/model/Cell.java
+++ src/main/model/Cell.java
@@ -14,4 +14,20 @@ public class Cell {
     public void setData(Object data) {
         this.data = data;
     }
+
+    public static Cell of(DataType type, String data) {
+        switch (type) {
+            case STRING:
+                return new Cell(data);
+            case NUMBER:
+                try {
+                    Double number = Double.parseDouble(data);
+                    return new Cell(number);
+                } catch (NumberFormatException e) {
+                    return null;
+                }
+            default:
+                return null;
+        }
+    }
 }
